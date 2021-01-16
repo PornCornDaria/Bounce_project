de00ce3e988edec2d317806ce91c6e4b4940056b
Daria <tarasova.alien@yandex.ru>
2021-01-16 21:18:13 +1000

    player in game

---
+++ player.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pygame import *
+import pygame
+
+MOVE_SPEED = 7
+WIDTH = 45
+HEIGHT = 35
+COLOR = "#888888"
+JUMP_POWER = 12
+GRAVITY = 0.5
+
+
+class Player(sprite.Sprite):
+	def __init__(self, x, y):
+		sprite.Sprite.__init__(self)
+		self.xvel = 0
+		self.startX = x
+		self.startY = y
+		self.image1 = pygame.image.load('C:/Users/Daria/PycharmProjects/bounce_project/ball1.jpg')
+		self.image = pygame.transform.scale(self.image1, (45, 35))
+		self.image.set_colorkey((255, 255, 255))
+		self.rect = Rect(x, y, WIDTH, HEIGHT)
+		self.yvel = 0
+		self.onGround = False
+	
+	def update(self, left, right, up, platforms):
+		if up:
+			if self.onGround:
+				self.yvel = -JUMP_POWER
+		if left:
+			self.xvel = -MOVE_SPEED  # Лево
+		
+		if right:
+			self.xvel = MOVE_SPEED  # Право
+		
+		if not (left or right):
+			self.xvel = 0
+		
+		if not self.onGround:
+			self.yvel += GRAVITY
+		
+		self.onGround = False
+		self.rect.y += self.yvel
+		self.collide(0, self.yvel, platforms)
+		
+		self.rect.x += self.xvel
+		self.collide(self.xvel, 0, platforms)
+	
+	def collide(self, xvel, yvel, platforms):
+		for p in platforms:
+			if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+				
+				if xvel > 0:  # если движется вправо
+					self.rect.right = p.rect.left
+				
+				if xvel < 0:  # если движется влево
+					self.rect.left = p.rect.right
+				
+				if yvel > 0:  # если падает вниз
+					self.rect.bottom = p.rect.top
+					self.onGround = True
+					self.yvel = 0
+				
+				if yvel < 0:  # если движется вверx
+					self.rect.top = p.rect.bottom
+					self.yvel = 0
---
+++ pygame_test.py
@@ -0,0 +1,90 @@
+import pygame
+from pygame import *
+from player import *
+from blocks import *
+
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 640
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
+BACKGROUND_COLOR = (100, 190, 255)
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode(DISPLAY)
+	pygame.display.set_caption("Bounce 2.0")
+	bg = Surface((WIN_WIDTH, WIN_HEIGHT))
+	bg.fill(BACKGROUND_COLOR)
+	
+	hero = Player(55, 55)
+	left = right = False
+	up = False
+	
+	entities = pygame.sprite.Group()
+	platforms = []
+	entities.add(hero)
+	
+	level = [
+		"--------------------------",
+		"-         --            - -",
+		"-                   --    - -",
+		"-                       - -",
+		"-             --        - --",
+		"-                       -",
+		"--                      -",
+		"-           ---            - ",
+		"- -                   --- -",
+		"-                        - --",
+		"-                       -",
+		"-      ---              -",
+		"- -                       -",
+		"-   -----------      -  - ",
+		"-                       -",
+		"-                -      -",
+		"-                   --  -",
+		"-    --                   -",
+		"-                       -",
+		"-------------------------"
+	]
+	
+	timer = pygame.time.Clock()
+	
+	while 1:
+		timer.tick(100)
+		screen.blit(bg, (0, 0))
+		for e in pygame.event.get():
+			if e.type == QUIT:
+				raise SystemExit
+			if e.type == KEYDOWN and e.key == K_LEFT:
+				left = True
+			if e.type == KEYDOWN and e.key == K_RIGHT:
+				right = True
+			if e.type == KEYUP and e.key == K_RIGHT:
+				right = False
+			if e.type == KEYUP and e.key == K_LEFT:
+				left = False
+			if e.type == KEYDOWN and e.key == K_UP:
+				up = True
+			if e.type == KEYUP and e.key == K_UP:
+				up = False
+		x = y = 0
+		for row in level:
+			for col in row:
+				if col == "-":
+					pf = Platform(x, y)
+					entities.add(pf)
+					platforms.append(pf)
+				
+				x += PLATFORM_WIDTH
+			y += PLATFORM_HEIGHT
+			x = 0
+			
+		hero.update(left, right, up, platforms)
+		entities.draw(screen)
+		pygame.display.flip()
+		pygame.display.update()
+
+
+if __name__ == "__main__":
+	main()
